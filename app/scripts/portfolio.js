3c8482b7cb7c91ed4145861e4965066b363a3f96
Kostya <kostgil@mail.ru>
2020-05-14 21:09:10 +0700

    np-v5

--- app/scripts/portfolio.js
+++ app/scripts/portfolio.js
@@ -1,18 +1,34 @@
 $(() => {
-	$(document).on('click', '.p-item-cover', function() {
+	$(document).on('click', '.p-item-cover', function () {
 		$(window).trigger('resize');
 	});
 
-	$('.modal-sl').slick({
-		dots: false,
-		infinite: false,
-		speed: 300,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		adaptiveHeight: true,
-		prevArrow: $('.fa-l'),
-		nextArrow: $('.fa-r')
+	// $('.modal-sl').each(function (i, item) {
+	// 	$(item).slick({
+	// 		dots: false,
+	// 		infinite: false,
+	// 		speed: 300,
+	// 		slidesToShow: 1,
+	// 		slidesToScroll: 1,
+	// 		adaptiveHeight: true,
+	// 		prevArrow: $(item).parent().find('.fa-l'),
+	// 		nextArrow: $(item).parent().find('.fa-r'),
+	// 	});
+	// });
+
+	$(document).on('opened', '.remodal', function () {
+		if (!$(this).find('.modal-sl').hasClass('slick-initialized')) {
+			$(this).find('.modal-sl').slick({
+				dots: false,
+				infinite: false,
+				speed: 300,
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				adaptiveHeight: true,
+				prevArrow: $(this).parent().find('.fa-l'),
+				nextArrow: $(this).parent().find('.fa-r'),
+			});
+		}
 	});
 });
-
 
