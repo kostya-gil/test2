d53998555d3680304ba581ac16fef1fdc116325e
Kostya <kostgil@mail.ru>
2020-05-16 19:07:10 +0700

    np-v6

--- app/blocks/header/header.js
+++ app/blocks/header/header.js
@@ -4,6 +4,16 @@ export default function header() {
 	let $item = null;
 
 	$(document).on('click', '.nav__link', function () {
+		const $attr = $(this).attr('data-link');
+		if (typeof $attr === typeof undefined && $attr === false) {
+			return false;
+		}
+		if (window.location.pathname !== '/') {
+			localStorage.setItem('link', $(this).attr('data-link'));
+			window.location.href = '/';
+		} else {
+			localStorage.removeItem('link');
+		}
 		if ($item) {
 			$item.removeClass('active');
 		}
@@ -30,6 +40,18 @@ export default function header() {
 			duration: 500
 		});
 		return false;
+	});
+
+	$(document).ready(function() {
+		if (localStorage.getItem('link') && window.location.pathname === '/') {
+			$(`[data-link=${localStorage.getItem('link')}]`).addClass('active');
+			$('html, body').animate({
+				scrollTop: $('.' + localStorage.getItem('link')).offset().top - $('.header').outerHeight()
+			}, {
+				duration: 500
+			});
+			localStorage.removeItem('link');
+		}
 	});
 
 	$(document).on('click', '.menu-btn', () => {
